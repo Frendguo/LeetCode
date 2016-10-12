ddbbb49f6ae98d18c2f6dd46da61eb4303f7bb2d
Frendguo <1293004688@qq.com>
2016-10-12 22:26:19 +0800

    字符串反转

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 问题描述：
+
+    Write a function that takes a string as input and returns the string reversed.
+
+    Example:
+    Given s = "hello", return "olleh".
+ */
+
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToString();
+            string s = ReverseString(str);
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+
+        public static string ReverseString(string s)
+        {
+            if (s == null || s.Length < 0)
+            {
+                return s;
+            }
+
+            char[] ch = s.ToArray();
+            int firstHalt = 0;
+
+            for (int i = ch.Length - 1; i >= ch.Length / 2; i--)
+            {
+                char tmp = ch[i];
+                ch[i] = ch[firstHalt];
+                ch[firstHalt++] = tmp;
+            }
+
+            return new string(ch);
+        }
+    }
+}
