40352a7a821b1195f4ac26d90f30a229f48757a4
Frendguo <1293004688@qq.com>
2016-10-13 15:15:27 +0800

    找single number 第一次尝试

---
+++ SingleNumber/SingleNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 问题描述：
+
+    Given an array of integers, every element appears twice except for one.
+    Find that single one.
+
+*/
+
+namespace SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 2, 1 };
+            Console.WriteLine("the single number is {0}!",SingleNumber(nums));
+            Console.Read();
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            List<int> list = nums.ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                int tmp = list[0];
+                list.RemoveAt(0);
+                if (!list.Contains(tmp))
+                {
+                    return tmp;
+                }
+            }
+            return 0;
+        }
+    }
+}
