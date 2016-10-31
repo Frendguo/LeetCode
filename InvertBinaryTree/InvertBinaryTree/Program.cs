cfea531d5de114147a45eb25710e012fc8f88313
Frendguo <1293004688@qq.com>
2016-10-31 19:28:53 +0800

    反转二叉树

---
+++ InvertBinaryTree/InvertBinaryTree/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+   
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            TreeNode t;
+            if (root == null)
+            {
+                return root;
+            }
+            if ((root.left == null)&&(root.right == null))
+            {
+                return root;
+            }
+            else if (root.left == null)
+            {
+                root.left = root.right;
+                root.right = null;
+            }
+            else if (root.right == null)
+            {
+                root.right = root.left;
+                root.left = null;
+            }
+            else
+            {
+                t = root.left;
+                root.left = root.right;
+                root.right = t;
+            }
+
+            root.left = InvertTree(root.left);
+            root.right = InvertTree(root.right);
+
+            return root;
+        }
+    }
+}
