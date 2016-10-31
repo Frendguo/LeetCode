f2f96975d90fe3b354a5787a8c8cb1b6fcf8dc71
Frendguo <1293004688@qq.com>
2016-10-31 13:22:45 +0800

    利用周期性实现

---
+++ AddDigits/AddDigits/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 问题描述：
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+    For example:
+
+    Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. 
+    Since 2 has only one digit, return it.
+
+    Follow up:
+    Could you do it without any loop/recursion in O(1) runtime?
+ * 
+*/
+namespace AddDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            Console.WriteLine("the solution is {0}", sol.AddDigits(38));
+            Console.Read();
+        }
+    }
+
+    public class Solution
+    {
+        public int AddDigits(int num)
+        {
+            int res = num % 9;
+            if (num <= 9) return num;
+            if (res == 0) return 9;
+            return res;
+        }
+    }
+}
---
+++ AddDigits/hnit.md
@@ -0,0 +1,5 @@
+#提示：
+
+因为结果为一位数，所以结果应具有周期性。
+
+还可参考以下链接：https://en.wikipedia.org/wiki/Digital_root
