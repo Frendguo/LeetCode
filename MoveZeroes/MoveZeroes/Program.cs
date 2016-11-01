e7f501dc1ac44bca5eaaf08d50d6240e2fecd964
Frendguo <1293004688@qq.com>
2016-11-01 09:42:53 +0800

    不复制数组的情况下移动数组内数据

---
+++ MoveZeroes/MoveZeroes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/**
+ * 问题描述：
+ * Given an array nums, 
+ * write a function to move all 0's to the end of it 
+ * while maintaining the relative order of the non-zero elements.
+
+    For example, given nums = [0, 1, 0, 3, 12], 
+        after calling your function,
+        nums should be [1, 3, 12, 0, 0].
+*/
+
+namespace MoveZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 1, 0, 3, 12 };
+            Solution sol = new Solution();
+            sol.MoveZeroes(nums);
+            foreach (int n in nums)
+            {
+                Console.WriteLine(n);
+            }
+            Console.Read();
+        }
+    }
+
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int n = 0; // 统计0的个数
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    n++;
+                }
+                else
+                {
+                    nums[i - n] = nums[i];
+                    if (n != 0)
+                    {
+                        nums[i] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
