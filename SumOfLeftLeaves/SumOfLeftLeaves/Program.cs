604137b082cb39e0720629ddeb27561c939e59b2
Frendguo <1293004688@qq.com>
2016-11-02 12:58:45 +0800

    二叉树的所有左叶子节点之和

---
+++ SumOfLeftLeaves/SumOfLeftLeaves/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfLeftLeaves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            if (root == null) return 0;
+            int sum = 0;
+            // left leaves
+            if ((root.left != null) &&
+                (root.left.left == null) &&
+                (root.left.right == null))
+            {
+                sum += root.left.val;
+            }
+
+            sum += SumOfLeftLeaves(root.left);
+            sum += SumOfLeftLeaves(root.right);
+
+            return sum;
+        }
+    }
+}
