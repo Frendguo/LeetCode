7115420c6823d8fbc0a4a78d80c8e67eb0a3e570
Frendguo <1293004688@qq.com>
2016-11-07 21:02:48 +0800

    增加数组方式实现

--- ConatainsDuplicate/ConatainsDuplicate/Program.cs
+++ ConatainsDuplicate/ConatainsDuplicate/Program.cs
@@ -29,15 +29,31 @@ namespace ConatainsDuplicate
 
         static bool ContainsDupliacate(int[] nums)
         {
-            /*方法一：采用Dictionary 实现*/
-            Dictionary<int, int> dictionary = new Dictionary<int, int>();
-            foreach (int n in nums)
+            ///*方法一：采用Dictionary 实现*/
+            //Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            //foreach (int n in nums)
+            //{
+            //    if (!dictionary.ContainsKey(n)) dictionary.Add(n, 1);
+            //    else return true;
+            //}
+            //return false;
+
+            /*方法二：采用数组实现（位操作）*/
+            // 15000 暂定
+            byte[] table = new byte[15000];
+            foreach (var item in nums)
             {
-                if (!dictionary.ContainsKey(n)) dictionary.Add(n, 1);
-                else return true;
+                // 为什么除以8 ？ 考虑到 byte 是8位
+                int i = item / 8;
+                int j = item % 8;
+                byte check =(byte) (1 << j);
+                if ((table[i] & check) != 0)
+                {
+                    return true;
+                }
+                table[i] |= check;
             }
             return false;
-
         }
     }
 }
