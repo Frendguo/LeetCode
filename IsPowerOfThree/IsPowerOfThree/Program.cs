e808ea8cffc02171566a812b11d4ad78fd4917dc
Frendguo <1293004688@qq.com>
2016-11-15 13:00:26 +0800

    判断给定的数是否为三的幂指数

---
+++ IsPowerOfThree/IsPowerOfThree/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 问题描述：
+ * Given an integer, write a function to determine（确定） if it is a power of three.
+
+    Follow up:
+    Could you do it without using any loop / recursion?
+**/
+
+namespace IsPowerOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (IsPowerOfThree(n))
+            {
+                Console.WriteLine("{0} is power of three!", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} isn't power of three!", n);
+            }
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 判断是否为三的幂
+        /// </summary>
+        /// <param name="n"></param>
+        /// 发现 三的幂数 的因子一定也是三的幂数
+        /// <returns></returns>
+        static bool IsPowerOfThree(int n)
+        {
+            if (n <= 0) return false;
+            int powerMax = (int)Math.Pow(3, (int)Math.Log(int.MaxValue, 3));
+            if (powerMax % n == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
